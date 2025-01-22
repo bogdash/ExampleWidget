246ca0236c3cdc9b154b2488cfd21f932d52636e
bogdash <ms.anastasia.bogdanova@gmail.com>
2025-01-22 18:02:43 +0300

    update: main ui

--- app/src/main/java/com/example/exaplewidget/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/exaplewidget/presentation/main/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.exaplewidget.R
 import com.example.exaplewidget.presentation.ui.theme.ExampleWidgetTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +27,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ExampleWidgetTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.linearGradient(
+                                colors = listOf(
+                                    Color(0xFF47BFDF),
+                                    Color(0xFF4A91FF)
+                                ),
+                                start = Offset(1000f, 0f),
+                                end = Offset(0f, 1000f)
+                            )
+                        ),
+                    containerColor = Color.Transparent,
+                ) { innerPadding ->
+                    MainContent(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +52,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainContent(modifier: Modifier = Modifier) {
+    Image(
+        imageVector = ImageVector.vectorResource(R.drawable.ic_cloud),
+        contentDescription = "Icon cloud"
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExampleWidgetTheme {
-        Greeting("Android")
-    }
 }
